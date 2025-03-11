bb30270ce1154e9efb2bda4590815223659e03f6
RAJENDIRAN <pranavan.rajendiran@capgemini.com>
2025-03-11 12:50:44 +0100

    adding a new component
    adding a new component about adn using it in the index page using Link

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import { StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href='/about'>À propos</Link>
     </View>
   );
 }
