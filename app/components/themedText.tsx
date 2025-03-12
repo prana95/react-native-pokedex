179ace7ff8f740fb1a046f10272c6b9cbd2bdecd
RAJENDIRAN <pranavan.rajendiran@capgemini.com>
2025-03-12 16:10:50 +0100

    added typography
    it means i added some style to the text which will be used in the project

---
+++ app/components/themedText.tsx
@@ -0,0 +1,59 @@
+import { Text, TextProps } from "react-native";
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create({
+    "body3": {
+        fontSize: 10,
+        lineHeight: 16,
+        fontWeight:"regular"
+    },
+    "headline": {
+        fontSize: 24,
+        lineHeight: 32,
+        fontWeight  : "bold"  
+    },
+    "subtitle3": {
+        fontSize: 10,
+        lineHeight: 16,
+        fontWeight  : "bold"
+        
+    },
+    "subtitle1": {
+        fontSize: 14,
+        lineHeight: 16,
+        fontWeight  : "bold"
+    },
+    "subtitle2": {
+        fontSize: 12,
+        lineHeight: 16,
+        fontWeight  : "bold"   
+    },
+    "body2": {
+        fontSize: 12,
+        lineHeight: 16,
+        fontWeight:"regular"   
+    },
+    "body1": {
+        fontSize: 14,
+        lineHeight: 16,
+        fontWeight:"regular"   
+    },
+    "caption": {
+        fontSize: 8,
+        lineHeight: 12,
+        fontWeight:"regular"   
+    }
+})
+
+type Props = TextProps &{ // The Props type extends TextProps from react-native. This means that Props will include all the properties that TextProps has and the additional properties also which is variant and color.
+    variant?:keyof typeof styles; // The line variant: keyof typeof styles; in your Props type definition means that the variant property can only be one of the keys of the styles object (for exemple:"body3", "headline", "subtitle3", "subtitle1", "subtitle2") . This ensures that the variant property must match one of the defined style names in the styles object.
+    color?:string
+}
+
+
+
+export function ThemedText({variant,color,...rest}:Props) {
+    return <Text style={styles[variant ?? 'body3']} {...rest}></Text>; // variant ?? 'body3' it means if the the variant value is null or undefined the defaut value  of variant will be 'body3'
+}
+
--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,11 @@ import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemedText } from "./components/themedText";
 export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href='/about'>À propos</Link>
-      <Link href={{pathname:'/pokemon/[id]',params:{id:3}}}>Pokemon 3</Link>
+      <ThemedText variant="headline" >Pokédex</ThemedText>
 
     </SafeAreaView>
   );
